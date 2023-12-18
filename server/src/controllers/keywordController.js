f0be2f4a34952e8220ab8442392b91f6e0888a8d
myat-hsu-mon <47687494+myat-hsu-mon@users.noreply.github.com>
2023-12-18 14:29:03 +0630

    update: file-upload using multer

--- server/src/controllers/keywordController.js
+++ server/src/controllers/keywordController.js
@@ -1,24 +1,18 @@
 const Keyword = require("../models/keywordModel");
 
-const getKeywords = async (req, res) => {
-  const keywords = await Keyword.findAll();
-  return res.status(200).json({
-    status: "success",
-    message: "All keywords are retrieved!",
-    data: keywords,
-  });
-};
-
-const getKeywordById = async (req, res) => {
-  
-};
+const getKeywordById = async (req, res) => {};
 
 const getKeywordByUserId = async (userId) => {};
 
 const getKeywordByPk = async (id) => {};
 
+const createKeyword = async (data) => {
+  const newKeyword = await Keyword.create(data);
+  return newKeyword;
+};
+
 module.exports = {
-  getKeywords,
   getKeywordByUserId,
   getKeywordById,
+  createKeyword,
 };
--- server/src/routes/uploadRoutes.js
+++ server/src/routes/uploadRoutes.js
@@ -1,7 +1,10 @@
+const multer = require("multer");
+const upload = multer({ dest: "uploads" });
+
 const router = require("express").Router();
 
 const uploadController = require("../controllers/uploadController");
 
-router.post("/", uploadController.uploadFile);
+router.post("/", upload.single("csv"), uploadController.uploadFile);
 
 module.exports = router;
