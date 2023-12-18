a30ffd3fd576c4894b775b32a0767ec9281775a2
myat-hsu-mon <47687494+myat-hsu-mon@users.noreply.github.com>
2023-12-18 18:49:02 +0630

    feat: create result model

---
+++ server/src/models/resultModel.js
@@ -0,0 +1,18 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../db");
+
+class Result extends Model {}
+Result.init(
+  {
+    totalLinks: DataTypes.INTEGER,
+    keywordId: DataTypes.INTEGER,
+    totalAdWords: DataTypes.INTEGER,
+    searchResults: DataTypes.STRING,
+    htmlCode: DataTypes.STRING,
+  },
+  { sequelize, modelName: "result" }
+);
+
+Result.sync({ force: true });
+
+module.exports = Result;
