5b74e01ee9e5ab199ac90ad36303303492245e1e
myat-hsu-mon <47687494+myat-hsu-mon@users.noreply.github.com>
2023-12-18 23:41:01 +0630

    update the interfaces

--- client/src/interfaces/searchResult.ts
+++ client/src/interfaces/searchResult.ts
@@ -1,58 +1,8 @@
-export interface SearchResult {
-  title: string
-  snippet: string
-  link: string
+export interface SearchResultProps {
+ totalLinks: number;
+ totalAdWords:number;
+ searchResults: string;
+ htmlCode: string;
 }
 
-export const searchResults: SearchResult[] = [
-  {
-    title: 'Example Title 1',
-    snippet: 'This is the snippet for example result 1.',
-    link: 'https://example.com/result1',
-  },
-  {
-    title: 'Example Title 2',
-    snippet: 'This is the snippet for example result 2.',
-    link: 'https://example.com/result2',
-  },
-  {
-    title: 'Example Title 3',
-    snippet: 'This is the snippet for example result 3.',
-    link: 'https://example.com/result3',
-  },
-  {
-    title: 'Example Title 4',
-    snippet: 'This is the snippet for example result 4.',
-    link: 'https://example.com/result4',
-  },
-  {
-    title: 'Example Title 5',
-    snippet: 'This is the snippet for example result 5.',
-    link: 'https://example.com/result5',
-  },
-  {
-    title: 'Example Title 6',
-    snippet: 'This is the snippet for example result 6.',
-    link: 'https://example.com/result6',
-  },
-  {
-    title: 'Example Title 7',
-    snippet: 'This is the snippet for example result 7.',
-    link: 'https://example.com/result7',
-  },
-  {
-    title: 'Example Title 8',
-    snippet: 'This is the snippet for example result 8.',
-    link: 'https://example.com/result8',
-  },
-  {
-    title: 'Example Title 9',
-    snippet: 'This is the snippet for example result 9.',
-    link: 'https://example.com/result9',
-  },
-  {
-    title: 'Example Title 10',
-    snippet: 'This is the snippet for example result 10.',
-    link: 'https://example.com/result10',
-  },
-]
+
