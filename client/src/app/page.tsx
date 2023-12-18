6214acb04c1f394005a46caba15a01fc41992f95
myat-hsu-mon <47687494+myat-hsu-mon@users.noreply.github.com>
2023-12-18 23:34:00 +0630

    Added protected page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,20 +1,23 @@
 'use client'
+import { useRouter } from 'next/navigation'
 import FileUpload from '@/components/FileUpload'
 import KeywordsList from '@/components/keywords/KeywordsList'
 import NoKeywordsMessage from '@/components/keywords/NoKeywordsMessage'
 import HTMLRender from '@/components/searchResults/HTMLRender'
 import ProcessingResultsMessage from '@/components/searchResults/ProcessingResultsMessage'
-import useSessionStorage from '@/hooks/useSessionStorage'
-import { useState } from 'react'
+import { useUser } from '@/contexts/UserContext'
+import useKeywordDetail from '@/hooks/useKeywordDetail'
+import useKeywordsByUserId from '@/hooks/useKeywordsByUserId'
 
 export default function Home() {
-  const [sessionValue] = useSessionStorage('user')
-  console.log({ sessionValue })
-  //useKeywords
-  //getSearchResultsbyKeywordId
+  const { keywords, setKeywords } = useKeywordsByUserId()
+  const { keyword, fetchKeywordDetail } = useKeywordDetail()
+  const router = useRouter()
 
-  const isProcessed = true
-  const [keywords, setKeywords] = useState<string[]>([])
+  const { user } = useUser()
+  if (!user) {
+    router.replace('/sign-in')
+  }
 
   return (
     <main className="m-auto flex min-h-screen max-w-7xl flex-col space-y-6 bg-white p-6">
@@ -23,21 +26,32 @@ export default function Home() {
         <NoKeywordsMessage />
       ) : (
         <div>
-          <div className="flex items-center">
-            <p className="mb-2 text-sm text-gray-500">
-              About 20,460,000,000 results (0.39 ms)
-            </p>
-            <p className="mb-2 ml-auto block rounded-md bg-gray-200 px-3 py-2 text-sm text-gray-600">
-              <span>Total Links - </span> 8000
-            </p>
-            <p className="mb-2 ml-2 block rounded-md bg-gray-200 px-3 py-2 text-sm text-gray-600">
-              <span>Total AdWords - </span> 300
-            </p>
-          </div>
-          <KeywordsList keywords={keywords} />
+          {keyword?.result && (
+            <div className="flex items-center">
+              <p className="mb-2 text-sm text-gray-500">
+                {keyword?.result?.searchResults}
+              </p>
+              <p className="mb-2 ml-auto block rounded-md bg-gray-200 px-3 py-2 text-sm text-gray-600">
+                <span>Total Links - </span>{' '}
+                <span>{keyword?.result?.totalLinks}</span>
+              </p>
+              <p className="mb-2 ml-2 block rounded-md bg-gray-200 px-3 py-2 text-sm text-gray-600">
+                <span>Total AdWords - </span> {keyword?.result?.totalAdWords}
+              </p>
+            </div>
+          )}
+
+          <KeywordsList
+            keywords={keywords}
+            fetchKeywordDetail={fetchKeywordDetail}
+          />
         </div>
       )}
-      {!isProcessed ? <ProcessingResultsMessage /> : <HTMLRender />}
+      {!keyword?.result ? (
+        <ProcessingResultsMessage />
+      ) : (
+        <HTMLRender htmlCode={keyword?.result.htmlCode} />
+      )}
     </main>
   )
 }
