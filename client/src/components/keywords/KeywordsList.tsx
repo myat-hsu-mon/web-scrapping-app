120bc39b82ccb5a1e9c3da8f92aee776fbe7f717
myat-hsu-mon <47687494+myat-hsu-mon@users.noreply.github.com>
2023-12-19 19:23:34 +0630

    update keyword list render

--- client/src/components/keywords/KeywordsList.tsx
+++ client/src/components/keywords/KeywordsList.tsx
@@ -11,22 +11,22 @@ const KeywordsList = ({
   keywords: KeywordProps[]
   fetchKeywordDetail: (id: number) => Promise<void>
 }) => {
-  const [activeBadge, setActiveBadge] = useState<null | number>(null)
+  const [activeBadge, setActiveBadge] = useState<KeywordProps>(keywords[0])
 
-  const handleActiveBadge = (keyword: KeywordProps, index: number) => {
-    setActiveBadge(index)
+  const handleActiveBadge = (keyword: KeywordProps) => {
+    setActiveBadge(keyword)
     fetchKeywordDetail(keyword.id)
   }
 
   return (
     <div className="max-h-64 overflow-y-auto rounded-md border border-gray-200 p-6">
       <ul className="flex flex-wrap gap-2" data-testid="keywords-list">
-        {keywords.map((keyword, index) => (
+        {keywords.map((keyword) => (
           <KeywordBadge
-            handleActiveBadge={() => handleActiveBadge(keyword, index)}
-            isActive={activeBadge === index}
+            handleActiveBadge={() => handleActiveBadge(keyword)}
+            isActive={activeBadge.id === keyword.id}
             keyword={keyword.name}
-            key={index}
+            key={keyword.id}
           />
         ))}
       </ul>
