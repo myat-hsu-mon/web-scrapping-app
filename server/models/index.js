defda5351ce74769c503fa557073fe0ba157cbc0
myat-hsu-mon <47687494+myat-hsu-mon@users.noreply.github.com>
2023-12-22 16:43:21 +0630

    feat: sequelize-cli init

---
+++ server/models/index.js
@@ -0,0 +1,61 @@
+"use strict";
+
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
+const process = require("process");
+const basename = path.basename(__filename);
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const db = {};
+
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD || null;
+const database = process.env.DB_NAME;
+const host = process.env.DB_HOST;
+const dialect = process.env.DB_DIALECT;
+
+const config = {
+  username,
+  password,
+  database,
+  host,
+  dialect,
+};
+
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
+
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return (
+      file.indexOf(".") !== 0 &&
+      file !== basename &&
+      file.slice(-3) === ".js" &&
+      file.indexOf(".test.js") === -1
+    );
+  })
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    );
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
