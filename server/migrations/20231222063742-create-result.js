30b9e6fbe540610ec4f127d7f12edadd3c30617c
myat-hsu-mon <47687494+myat-hsu-mon@users.noreply.github.com>
2023-12-22 16:47:33 +0630

    feat: create result model and migration

---
+++ server/migrations/20231222063742-create-result.js
@@ -0,0 +1,42 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, DataTypes) {
+    await queryInterface.createTable("results", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      keywordId: {
+        type: DataTypes.INTEGER,
+      },
+      totalLinks: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
+      totalAdWords: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
+      searchResults: {
+        type: DataTypes.STRING,
+      },
+      htmlCode: {
+        type: DataTypes.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    });
+  },
+  async down(queryInterface, DataTypes) {
+    await queryInterface.dropTable("results");
+  },
+};
