9c9614f106302b6cf89966f63c74ef51ef333a5a
myat-hsu-mon <47687494+myat-hsu-mon@users.noreply.github.com>
2023-12-23 12:28:09 +0630

    fix: fix protected page warning

--- client/src/components/common/ProtectedPage.tsx
+++ client/src/components/common/ProtectedPage.tsx
@@ -1,18 +1,38 @@
 'use client'
-import { setAuthorizationHeader } from '@/helper/setAuthorizationHeader'
+import { ReactNode, useEffect } from 'react'
 import { useRouter } from 'next/navigation'
-import { ReactNode } from 'react'
+
+import { useUser } from '@/contexts/UserContext'
+import { setAuthorizationHeader } from '@/helper/setAuthorizationHeader'
 
 function ProtectedPage({ children }: { children: ReactNode }) {
-  const userJSON = sessionStorage.getItem('user')
-  const parsedUser = userJSON ? JSON.parse(userJSON) : null
   const router = useRouter()
+  const { setUserData } = useUser()
 
-  if (parsedUser) {
-    setAuthorizationHeader(parsedUser.token)
-  } else {
-    router.replace('/sign-in')
-  }
+  useEffect(() => {
+    const userJSON = sessionStorage.getItem('user')
+    const parsedUser = userJSON ? JSON.parse(userJSON) : null
+
+    if (parsedUser) {
+      const {
+        user: { id, name, email, password },
+        token,
+      } = parsedUser
+      setAuthorizationHeader(parsedUser.token)
+      setUserData(
+        {
+          id,
+          user: name,
+          email,
+          password,
+        },
+        token,
+      )
+    } else {
+      router.replace('/sign-in')
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return <>{children}</>
 }
