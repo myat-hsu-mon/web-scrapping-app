e7ee54d508b48a2654ad6e33f73c0b31955daca5
myat-hsu-mon <47687494+myat-hsu-mon@users.noreply.github.com>
2023-12-24 08:48:10 +0630

    feat: handle google-rate-limiting using rabbitmq-delayed-message-exchange

--- server/src/queues/consumer.js
+++ server/src/queues/consumer.js
@@ -3,22 +3,39 @@ const { scrapeWebData } = require("../web-scrape/scrape");
 const { createResult } = require("../controllers/resultController");
 
 async function consumeQueue() {
-  const { channel } = await connect();
-  const queueName = "keywordsQueue";
+  try {
+    const { channel } = await connect();
+    const exchangeName = "keywordsExchange";
+    const queueName = "keywordsQueue";
+    // Create the delayed exchange
+    await channel.assertExchange(exchangeName, "x-delayed-message", {
+      autoDelete: false,
+      durable: true,
+      passive: true,
+      arguments: { "x-delayed-type": "direct" },
+    });
 
-  await channel.assertQueue(queueName, { durable: false });
+    // Create the queue
+    await channel.assertQueue(queueName, { durable: true });
 
-  console.log(`Waiting for messages from queue '${queueName}'...`);
+    // Bind the queue to the exchange
+    await channel.bindQueue(queueName, exchangeName, "");
 
-  channel.consume(queueName, async (message) => {
-    if (message !== null) {
-      const { name, id } = JSON.parse(message.content.toString());
-      const data = await scrapeWebData(name);
-      console.log(`Received message: ${message.content.toString()}`);
-      createResult({ keywordId: id, ...data });
-      channel.ack(message);
-    }
-  });
+    console.log(`Waiting for messages from queue '${queueName}'...`);
+
+    // Consume messages
+    channel.consume(queueName, async (message) => {
+      if (message !== null) {
+        const { name, id } = JSON.parse(message.content.toString());
+        const data = await scrapeWebData(name);
+        console.log(`Received message: ${message.content.toString()}`);
+        createResult({ keywordId: id, ...data });
+        channel.ack(message);
+      }
+    });
+  } catch (error) {
+    console.error("Error:", error);
+  }
 }
 
 consumeQueue();
